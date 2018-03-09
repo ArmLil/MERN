e3ce676e0897955d3b1c4f32855a4a40e1f0c2d8
ArmLil <armlilhov@mail.ru>
2018-03-09 20:50:14 +0400

    Integrating the database with back-end

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = require('./routes/routes.js')
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+const mongoose = require('mongoose');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+mongoose.connect('mongodb://<lilit>:<immalabicackanunne1977>@ds261088.mlab.com:61088/mern');
 
 app.use('/', router);
 
